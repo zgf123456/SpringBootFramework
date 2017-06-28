bde19ccfd6c9d920ffd6a159b3e7b7ee96e9e6ac
zgf <guofengzheng1@creditease.cn>
2017-06-28 17:41:55 +0800

    add sample shiro

---
+++ fastdev-samples/fastdev-sample-shiro/src/main/java/com/fastdev/sample/shiro/service/UserInfoService.java
@@ -0,0 +1,15 @@
+package com.fastdev.sample.shiro.service;
+
+import com.fastdev.sample.shiro.model.UserInfo;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by zgf on 17/6/28.
+ */
+@Service
+public class UserInfoService {
+    public UserInfo findByUsername(String username) {
+        return null;
+    }
+
+}
